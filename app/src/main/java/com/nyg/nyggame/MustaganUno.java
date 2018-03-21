bcec3cf40b817af300cf774cdd34b33cf9552bfa
Noelia <noelia.herranz@hotmail.es>
2018-03-21 17:39:57 +0100

    MustaganUno

--- app/src/main/java/com/nyg/nyggame/MustaganUno.java
+++ app/src/main/java/com/nyg/nyggame/MustaganUno.java
@@ -1,8 +1,27 @@
 package com.nyg.nyggame;
 
-/**
- * Created by Noelia on 20/03/2018.
- */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MustaganUno {
+public class MustaganUno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.no_choose);
+
+        CommonFunctions.setImage((ImageView)findViewById(R.id.image), R.drawable.img_mustagan_uno);
+        CommonFunctions.setText((TextView)findViewById(R.id.text), R.string.txt_mustagan_uno);
+        CommonFunctions.setButton((Button)findViewById(R.id.button), R.string.btn_mustagan_uno);
+    }
+
+    public void nextActivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), MustaganDos.class);
+        startActivity(intent);
+    }
 }
